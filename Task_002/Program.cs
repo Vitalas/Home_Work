fc4035707e329a2a4f50bcf91f59dd05b08fb16e
Vitalas <Vitalas_2004@mail.ru>
2023-09-19 19:52:22 +0300

    Сделал 2 задачу

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -2,4 +2,16 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
- 
+
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2)
+    Console.WriteLine("Не корректный ввод чисел");
+else if (number1 > number2)
+    Console.WriteLine($"{number1} > {number2}");
+else Console.WriteLine($"{number2} > {number1}");
+
+
+
